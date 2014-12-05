95cd80587efcf2986884b57917019b3332ff7093
Lukas Kollmann <lukas-kollmann@gmx.at>
2014-12-05 21:41:25 +0100

    1

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+/* globals module, require */
+
+module.exports = function(grunt) {
+
+  "use strict";
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+
+    uglify: {
+      global: {
+        files: {
+          "js/site.min.js": ["js/site.js"]
+        }
+      }
+    },
+
+    sass: {
+      global: {
+        options: {
+          style: "compressed",
+          compass: true,
+          require: ["susy", "breakpoint"]
+        },
+        files: {
+          "css/global-unprefixed.css": "sass/global.scss"
+        }
+      }
+    },
+
+    autoprefixer: {
+      global: {
+        src: "css/global-unprefixed.css",
+        dest: "css/global.css"
+      }
+    },
+
+    shell: {
+      jekyllServe: {
+        command: "jekyll serve --baseurl="
+      },
+      jekyllBuild: {
+        command: "jekyll build --config _config.yml"
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      site: {
+        files: ["index.html", "writing.html", "about.html", "_layouts/*.html", "_posts/*.md", "_projects/*.md", "_includes/*.html"],
+        tasks: ["shell:jekyllBuild"]
+      },
+      js: {
+        files: ["js/*.js"],
+        tasks: ["uglify", "shell:jekyllBuild"]
+      },
+      css: {
+        files: ["sass/*.scss"],
+        tasks: ["sass", "autoprefixer", "shell:jekyllBuild"]
+      },
+      svgIcons: {
+        files: ["svg/*.svg"],
+        tasks: ["svgstore", "shell:jekyllBuild"]
+      }
+    },
+
+    svgstore: {
+      options: {
+        prefix : "shape-",
+        cleanup: false,
+        svg: {
+          style: "display: none;"
+        }
+      },
+      default: {
+        files: {
+          "_includes/svg-defs.svg": ["svg/*.svg"]
+        }
+      }
+    }
+
+  });
+
+  require("load-grunt-tasks")(grunt);
+
+  grunt.registerTask("serve", ["shell:jekyllServe"]);
+  grunt.registerTask("default", ["sass", "autoprefixer", "svgstore", "shell:jekyllBuild", "watch"]);
+
+};
